601f23e7324b9a497de9756f0b55901d3110fde6
2litrewt <buicnuwe0170@gmail.com>
2024-11-21 20:31:24 +0900

    nextjs_app

--- Next.js/nextjs_app/nextjs-dashboard/app/lib/data.ts
+++ Next.js/nextjs_app/nextjs-dashboard/app/lib/data.ts
@@ -21,7 +21,7 @@ export async function fetchRevenue() {
 
     console.log('Data fetch completed after 3 seconds.');
 
-    return invoice[0];
+  
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
